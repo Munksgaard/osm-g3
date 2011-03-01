84c540d50bcea9aa0e7523c447ee74b0032bc5bd
Daniel Egeberg <daniel.egeberg@gmail.com>
2011-03-01 12:59:08 +0100

    fix indentering og fjern udkommenteret kode

--- buenos/kernel/lock_cond.h
+++ buenos/kernel/lock_cond.h
@@ -5,11 +5,6 @@
 
 typedef int* cond_t;
 
-/*typedef struct cond_t
-{
-  
-} cond_t;*/
-
 int condition_reset(cond_t *cond);
 void condition_wait(cond_t *cond, lock_t *condition_lock);
 void condition_signal(cond_t *cond, lock_t *condition_lock);
